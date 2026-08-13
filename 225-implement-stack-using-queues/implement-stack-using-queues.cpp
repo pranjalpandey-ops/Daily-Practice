fb06895a33fd4d4a0b2be7668b954b0ca5002958
pranjalpandey-ops <pranjalkumarpandey9a27@gmail.com>
2026-08-13 21:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (17.83%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+    queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        while (n-- > 1) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
