3b836fb4ace7d44ba0209eaecc7d434af539da33
pranjalpandey-ops <pranjalkumarpandey9a27@gmail.com>
2026-09-14 21:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.03%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
+        if (r1[2] <= r2[0] || r2[2] <= r1[0])
+            return false;
+
+        if (r1[3] <= r2[1] || r2[3] <= r1[1])
+            return false;
+
+        return true;
+    }
+};
